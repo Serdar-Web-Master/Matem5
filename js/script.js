581961febfb38d3ac254ff306ff2b70d1da4c60c
Serdar <serdarcholukov@yandex.com>
2022-11-12 17:44:23 +0500

    6

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 import { a, b} from "./vars.js";
-
 console.log(a,b);
+
+const inputNumber = document.querySelector('.input__number');
+const btn = document.querySelector('.btn');
+console.log(inputNumber.value);
+console.log(inputNumber);
+
+btn.addEventListener("click",goTo)
+
+const answerBlock = document.querySelector('.answer__block');
+
+function goTo() {
+   answerBlock.classList.remove("none");
+console.log(inputNumber.value);
+}
+
+
+// answerBlock.innerHTML = "<p>составляем уравнение :</p> "
+
+// answerBlock.insertAdjacentHTML("afterbegin",
+// '<p> <span class="task__number">86.</span>  В магазин привезли 2400 кг фруктов: 120 ящиков груш и 140 ящиков яблок. Известно, что масса одного ящика яблок в 2 раза больше, чем масса одного ящика груш. Сколько килограммов груш привезли в магазин?</p> <h1>Решение</h1> <p>Пусть масса одного ящика груш Х кг,тогда масса одного ящика яблок будет 2Х кг,так как привезли всего 2400 кг </p>')
